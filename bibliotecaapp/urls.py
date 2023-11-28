a720676608f31594dfad7046a19c47d8f32bcec3
carlosmf996 <carlosm996@gmail.com>
2023-11-28 10:25:29 +0100

    delete correcto, falta nuevo libro y editar

--- bibliotecaapp/urls.py
+++ bibliotecaapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-from .views import LibroList
+from .views import LibroList, New, LibroDetails, LibroDelete
 
 urlpatterns = [
     path('',LibroList.as_view(), name='libro_list'),
+    path('new', New.as_view(), name ='new'),
+    path('libro/<int:pk>/edit/', LibroDetails.as_view(), name='libro_details'),
+    path('libro/<int:pk>/delete/', LibroDelete.as_view(), name='libro_delete'),
 ]
--- bibliotecaapp/views.py
+++ bibliotecaapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView, FormView
 from .models import Libro, Usuario, Autor, Editorial, Prestamo
 
 # Create your views here.
@@ -8,3 +9,21 @@ class LibroList(ListView):
     
     model = Libro
     template_name = 'biblioteca/libro_list.html'
+
+class New(FormView):
+
+    model = Libro
+    template_name = 'biblioteca/new.html'
+    fields = []
+    success_url = reverse_lazy("libro_list")
+
+class LibroDetails(DetailView):
+
+    model = Libro
+    template_name = 'biblioteca/libro_details.html'
+
+class LibroDelete(DeleteView):
+
+    model = Libro
+    template_name = 'biblioteca/libro_delete.html'
+    success_url = reverse_lazy("libro_list")
