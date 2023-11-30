5e55a7d63f73bcccef559bb65952fb8271be47bc
carlosmf996 <carlosm996@gmail.com>
2023-11-30 13:10:51 +0100

    No va el confirmar reserva con sus muertos

--- bibliotecaapp/urls.py
+++ bibliotecaapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import LibroList, New, LibroDetails, LibroDelete, Edit
+from .views import LibroList, New, LibroDetails, LibroDelete, Edit, Reserva, ConfirmarReserva
 
 urlpatterns = [
     path('',LibroList.as_view(), name='libro_list'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('libro/<int:pk>/', LibroDetails.as_view(), name='libro_details'),
     path('libro/<int:pk>/delete/', LibroDelete.as_view(), name='libro_delete'),
     path('libro/<int:pk>/edit/', Edit.as_view(), name='libro_edit'),
+    path('libro/<int:pk>/reserva/', Reserva.as_view(), name='libro_reserva'),
+    path('libro/<int:libro_id>/confirmar/', ConfirmarReserva.as_view(), name='libro_confirmar_reserva'),
 ]
--- bibliotecaapp/views.py
+++ bibliotecaapp/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import redirect, render
@@ -68,3 +69,30 @@ class Edit(UpdateView):
     fields = ["titulo", "autor", "editorial", "rating", "fecha_publicacion", "genero", "isbn", "resumen", "disponibilidad", "portada"]
     template_name = 'biblioteca/libro_edit.html'
     success_url = reverse_lazy("libro_list")
+
+
+class Reserva(DetailView):
+
+    model = Libro
+    fields = ["titulo", "autor", "editorial"]
+    template_name = 'biblioteca/libro_reserva.html'
+
+class ConfirmarReserva(View):
+
+    def get(self, request, libro_id):
+        libro = Libro.objects.get(pk=libro_id)
+        return render(request, 'biblioteca/libro_list.html', {'libro': libro})
+
+    def post(self, request, libro_id):
+        libro = Libro.objects.get(pk=libro_id)
+
+        prestamo = Prestamo(
+            titulo=libro,
+            fechaPrestamo=date.today(),
+            fechaDevolucion=None,  
+            usuario=request.user,
+            estado='PRE'  
+        )
+        prestamo.save()
+
+        return redirect('libro_list')
